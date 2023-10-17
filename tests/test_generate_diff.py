fc40813a4086421e5a8f10d52a67ce53faec83cf
ImTsukushi <cucker30000@gmail.com>
2023-10-17 12:33:53 +0300

    step 4

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,5 @@
 import pytest
-from gendiff.diffgenerator import generate_diff
+from gendiff import diffgenerator
 
 with open('./tests/fixtures/expected_results/stylish-flat.txt', mode='r') as file:
     expected = file.read()
@@ -11,4 +11,4 @@ file_path_2 = './tests/fixtures/trees/file2.json'
 @pytest.mark.parametrize('file_path_1, file_path_2, expected',
                          [(file_path_1, file_path_2, expected)])
 def test_generate_diff(file_path_1, file_path_2, expected):
-    assert (generate_diff(file_path_1, file_path_2) == expected)
+    assert (diffgenerator.generate_diff(file_path_1, file_path_2) == expected)
